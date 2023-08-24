24163ef6a39a112ad7abc484c781b8b8c9dda838
Agnes4Him <ojuhagnes@gmail.com>
2023-08-24 11:02:15 +0100

    First commit message.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
     //const url = 'backend'     for docker-compose file
     //const url = '/backend'    for k8s with ingess rule
-    //const url 'backend:9000'  for k8s with NodePort as service type for frontend and mongo-express 
+    //const url = 'backend:9000'  for k8s with NodePort as service type for frontend and mongo-express 
     const url = 'http://localhost:9000' // for local development
     const handleFormSubmit = (e) => {
         e.preventDefault()
